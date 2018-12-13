03448cf01e86c39e886e7df4952ef72fd6c7aafb
Hikaru <takahito2takahito@gmail.com>
2018-12-13 23:22:49 +0900

    2018/12/13　第3回コミット

--- app/src/main/java/twentyfour_seconds/com/del/TopActivity.java
+++ app/src/main/java/twentyfour_seconds/com/del/TopActivity.java
@@ -1,129 +1,45 @@
 package twentyfour_seconds.com.del;
 
-import android.support.constraint.ConstraintLayout;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.ViewFlipper;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class TopActivity extends AppCompatActivity {
-    //テスト
-    public ScrollView Scrollview;
-    public ConstraintLayout ConstraintLayout;
-    //test
-    public ViewFlipper viewFlipper;
-    //フリックのＸ位置
-    private float X,firstX;
-    // フリックの遊び部分（最低限移動しないといけない距離）
-    private float adjust = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_top);
+        setContentView(R.layout.kamoshidawork);
 
-        viewFlipper = (ViewFlipper) findViewById(R.id.flipper);
-        ConstraintLayout = findViewById(R.id.constraintinscroll);
-        Scrollview = findViewById(R.id.scrollView4);
-    }
-//        ScrollView.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                System.out.println("RelativeLayout");
-//                return false;
-//            }
-//        });
+        //横スクロールに入るimageviewの横幅をプログラムより指定
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        Display disp = wm.getDefaultDisplay();
 
-//        ConstraintLayout.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                Log.d("試し" , "ConstraintLayout");
-//                return false;
-//            }
-//        });
-//
-//        viewFlipper.setOnTouchListener(new View.OnTouchListener() {
-//                                           @Override
-//                                           public boolean onTouch(View v, MotionEvent event) {
-//                                               switch (event.getAction()) {
-//                                                   case MotionEvent.ACTION_DOWN://タッチ押下
-//                                                       firstX = event.getX();
-//                                                       Log.d("X", "" + firstX);
-//                                                       break;
-//                                                   case MotionEvent.ACTION_UP://指を持ち上げた場合
-//                                                       X = event.getX();
-//                                                       Log.d("X", "" + X);
-//                                                       // Handling left to right screen swap.
-//                                                       if (X - firstX > adjust) {
-//                                                           // Next screen comes in from left.
-//                                                           viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_from_left));
-//                                                           // Current screen goes out from right.
-//                                                           viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_out_to_right));
-//
-//                                                           // Display next screen.
-//                                                           viewFlipper.showNext();
-//                                                       }
-//
-//                                                       // Handling right to left screen swap.
-//                                                       else if (firstX - X > adjust) {
-//                                                           // Next screen comes in from right.
-//                                                           viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_from_right));
-//                                                           // Current screen goes out from left.
-//                                                           viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_out_to_left));
-//
-//                                                           // Display previous screen.
-//                                                           viewFlipper.showPrevious();
-//                                                       }
-//                                                       break;
-//                                               }
-//                                               return false;
-////            }
-////        });
-//                                           }
-//                                       });
+        //画像3枚の横幅を取得する
+        final ImageView img12 = (ImageView) findViewById(R.id.imageView12);
+        final ImageView img13 = (ImageView) findViewById(R.id.imageView13);
+        final ImageView img14 = (ImageView) findViewById(R.id.imageView14);
+        ViewGroup.LayoutParams params12 = img12.getLayoutParams();
+        ViewGroup.LayoutParams params13 = img12.getLayoutParams();
+        ViewGroup.LayoutParams params14 = img12.getLayoutParams();
+        //画面サイズを取得
+        Point realSize = new Point();
+        disp.getRealSize(realSize);
 
-    // Using the following method, we will handle all screen swaps.
-    public boolean onTouchEvent(MotionEvent touchevent) {
-//        ScrollView ScrollView = findViewById(R.id.scrollView4);
-//        ScrollView.requestDisallowInterceptTouchEvent(true);
-        switch (touchevent.getAction()) {
-            case MotionEvent.ACTION_DOWN://タッチ押下
-                firstX = touchevent.getX();
-                Log.d("X", ""+ firstX);
-                break;
-            case MotionEvent.ACTION_UP://指を持ち上げた場合
-                X = touchevent.getX();
-                Log.d("X", ""+ X);
-                // Handling left to right screen swap.
-                if (X - firstX > adjust) {
-                    // Next screen comes in from left.
-                    viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_from_left));
-                    // Current screen goes out from right.
-                    viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_out_to_right));
+        int realScreenWidth = realSize.x;
+        int realScreenHeight = realSize.y;
 
-                    // Display next screen.
-                    viewFlipper.showNext();
-                }
-
-                // Handling right to left screen swap.
-                else if (firstX - X > adjust) {
-                    // Next screen comes in from right.
-                    viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_from_right));
-                    // Current screen goes out from left.
-                    viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_out_to_left));
-
-                    // Display previous screen.
-                    viewFlipper.showPrevious();
-                }
-                break;
-        }
-        return false;
+        //  横幅のみ画面サイズに変更
+        params12.width = realScreenWidth;
+        img12.setLayoutParams(params12);
+        params13.width = realScreenWidth;
+        img13.setLayoutParams(params13);
+        params14.width = realScreenWidth;
+        img14.setLayoutParams(params14);
     }
 }
+
