b27acb51972fe658e72a5b83ceb9333e501c67cf
Hikaru <takahito2takahito@gmail.com>
2018-12-13 23:12:47 +0900

    2018/12/13　第2回コミット

--- app/src/main/java/twentyfour_seconds/com/del/kamoshidawork.java
+++ app/src/main/java/twentyfour_seconds/com/del/kamoshidawork.java
@@ -1,11 +1,13 @@
 package twentyfour_seconds.com.del;
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.view.MotionEvent;
-import android.view.animation.AnimationUtils;
-import android.widget.ViewFlipper;
+import android.view.Display;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class kamoshidawork extends AppCompatActivity {
 
@@ -13,5 +15,32 @@ public class kamoshidawork extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kamoshidawork);
+
+        //横スクロールに入るimageviewの横幅をプログラムより指定
+        WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+        Display disp = wm.getDefaultDisplay();
+
+        //画像3枚の横幅を取得する
+        final ImageView img12 = (ImageView) findViewById(R.id.imageView12);
+        final ImageView img13 = (ImageView) findViewById(R.id.imageView13);
+        final ImageView img14 = (ImageView) findViewById(R.id.imageView14);
+        ViewGroup.LayoutParams params12 = img12.getLayoutParams();
+        ViewGroup.LayoutParams params13 = img12.getLayoutParams();
+        ViewGroup.LayoutParams params14 = img12.getLayoutParams();
+        //画面サイズを取得
+        Point realSize = new Point();
+        disp.getRealSize(realSize);
+
+        int realScreenWidth = realSize.x;
+        int realScreenHeight = realSize.y;
+
+        //  横幅のみ画面サイズに変更
+        params12.width = realScreenWidth;
+        img12.setLayoutParams(params12);
+        params13.width = realScreenWidth;
+        img13.setLayoutParams(params13);
+        params14.width = realScreenWidth;
+        img14.setLayoutParams(params14);
     }
 }
+
