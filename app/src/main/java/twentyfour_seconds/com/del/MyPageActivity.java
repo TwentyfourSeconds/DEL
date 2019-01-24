d6d55530904bca4abdb4ace89d023abf2108b128
Hikaru <takahito2takahito@gmail.com>
2019-01-24 23:11:13 +0900

    画面遷移1

--- app/src/main/java/twentyfour_seconds/com/del/MyPageActivity.java
+++ app/src/main/java/twentyfour_seconds/com/del/MyPageActivity.java
@@ -3,6 +3,7 @@ package twentyfour_seconds.com.del;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -43,6 +44,52 @@ public class MyPageActivity extends AppCompatActivity {
         age.setText("29才");
         entryTrust.setText("50");
         planTrust.setText("50");
+
+
+        //下部メニューボタンを押下したときの処理を記載
+        ImageView menu_bar_home = findViewById(R.id.mypage_tab).findViewById(R.id.menu_bar_home);
+        ImageView menu_bar_event = findViewById(R.id.mypage_tab).findViewById(R.id.menu_bar_event);
+        ImageView menu_bar_chat = findViewById(R.id.mypage_tab).findViewById(R.id.menu_bar_chat);
+        ImageView menu_bar_mypage = findViewById(R.id.mypage_tab).findViewById(R.id.menu_bar_mypage);
+
+        MyPageActivity.menuClickListener menuClickListener = new MyPageActivity.menuClickListener();
+
+        menu_bar_home.setOnClickListener(menuClickListener);
+        menu_bar_event.setOnClickListener(menuClickListener);
+        menu_bar_chat.setOnClickListener(menuClickListener);
+        menu_bar_mypage.setOnClickListener(menuClickListener);
+
+    }
+
+    public class menuClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            int id = view.getId();
+            switch(id){
+                case R.id.menu_bar_home:
+                    //home画面へと飛ぶ処理
+                    Intent intentHome = new Intent(MyPageActivity.this, TopActivity.class);
+                    startActivity(intentHome);
+                    break;
+                case R.id.menu_bar_event:
+                    //イベント作成画面へと飛ぶ処理
+                    Intent intentEvent = new Intent(MyPageActivity.this, tabcontrol_main.class);
+                    startActivity(intentEvent);
+                    break;
+                case R.id.menu_bar_chat:
+                    //チャット画面へと飛ぶ処理
+                    Intent intentchat = new Intent(MyPageActivity.this, ChatDB.class);
+                    startActivity(intentchat);
+                    break;
+                case R.id.menu_bar_mypage:
+                    //マイページ画面へと飛ぶ処理
+                    Intent intentMypage = new Intent(MyPageActivity.this, MyPageActivity.class);
+                    startActivity(intentMypage);
+                    break;
+            }
+        }
+
+
 
 //        Intent intent = getIntent();
 //        int iconId = getResources().getIdentifier(intent.getStringExtra("icon"), "drawable", getPackageName());
--- app/src/main/java/twentyfour_seconds/com/del/TopActivity.java
+++ app/src/main/java/twentyfour_seconds/com/del/TopActivity.java
@@ -1,6 +1,7 @@
 package twentyfour_seconds.com.del;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -48,7 +49,54 @@ public class TopActivity extends AppCompatActivity {
         img13.setLayoutParams(params13);
         params14.width = realScreenWidth;
         img14.setLayoutParams(params14);
+
+        //下部メニューボタンを押下したときの処理を記載
+        ImageView menu_bar_home = findViewById(R.id.tab1).findViewById(R.id.menu_bar_home);
+        ImageView menu_bar_event = findViewById(R.id.tab1).findViewById(R.id.menu_bar_event);
+        ImageView menu_bar_chat = findViewById(R.id.tab1).findViewById(R.id.menu_bar_chat);
+        ImageView menu_bar_mypage = findViewById(R.id.tab1).findViewById(R.id.menu_bar_mypage);
+
+        menuClickListener menuClickListener = new menuClickListener();
+
+        menu_bar_home.setOnClickListener(menuClickListener);
+        menu_bar_event.setOnClickListener(menuClickListener);
+        menu_bar_chat.setOnClickListener(menuClickListener);
+        menu_bar_mypage.setOnClickListener(menuClickListener);
+
     }
+
+    public class menuClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            int id = view.getId();
+            switch(id){
+                case R.id.menu_bar_home:
+                    //home画面へと飛ぶ処理
+                    Intent intentHome = new Intent(TopActivity.this, TopActivity.class);
+                    startActivity(intentHome);
+                    break;
+                case R.id.menu_bar_event:
+                    //イベント作成画面へと飛ぶ処理
+                    Intent intentEvent = new Intent(TopActivity.this, tabcontrol_main.class);
+                    startActivity(intentEvent);
+                    break;
+                case R.id.menu_bar_chat:
+                    //チャット画面へと飛ぶ処理
+                    Intent intentchat = new Intent(TopActivity.this, ChatDB.class);
+                    startActivity(intentchat);
+                    break;
+                case R.id.menu_bar_mypage:
+                    //マイページ画面へと飛ぶ処理
+                    Intent intentMypage = new Intent(TopActivity.this, MyPageActivity.class);
+                    startActivity(intentMypage);
+                    break;
+            }
+        }
+    }
+
+
+
+
 }
 
 
