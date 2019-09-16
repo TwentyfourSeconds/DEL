f1940626e2de3d3dca7bbca353f522db96e7aed0
geeta333 <gusigusi_gusi@yahoo.co.jp>
2019-09-16 21:53:12 +0900

    2019/09/16 21:53

--- app/src/main/java/twentyfour_seconds/com/del/util/Common.java
+++ app/src/main/java/twentyfour_seconds/com/del/util/Common.java
@@ -17,8 +17,8 @@ import java.util.List;
 
 public class Common {
 
-    public static final String STR_MYSQL_URL = "http://3.15.24.173";
-//    public static final String STR_MYSQL_URL = "http://10.0.2.2";
+//    public static final String STR_MYSQL_URL = "http://3.15.24.173";
+    public static final String STR_MYSQL_URL = "http://10.0.2.2";
     public static final String EVENT_INFO_MYSQL_URL = STR_MYSQL_URL + ":8000/event_info_id_search";
     public static final String EVENT_SEARCH_NAME_URL = STR_MYSQL_URL + ":8000/event_info_event_name_search";
     public static final String EVENT_SEARCH_TAG_URL = STR_MYSQL_URL + ":8000/recruitment_tagMap";
@@ -31,6 +31,16 @@ public class Common {
     public static final String ADD_PARTICIPANT_EVENT_URL = STR_MYSQL_URL + ":3051/add_participant_event";
     public static final String CREATE_EVENT_URL = STR_MYSQL_URL + ":4050/create_event";
     public static final String UPDATE_EVENT_URL = STR_MYSQL_URL + ":4051/update_event";
+
+    public static final String[] REGION_ARY = { "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+            "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県", "富山県",
+            "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県",
+            "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県",
+            "山口県", "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県",
+            "大分県", "宮崎県", "鹿児島県", "沖縄県"};
+    public static final String REGION_NOTHING = "00000000000000000000000000000000000000000000000";
+    public static final String REGION_FLG_ON = "1";
+    public static final String REGION_FLG_OFF = "0";
 
     public static int total;
     public static List<String> idList = new ArrayList<>();
@@ -170,7 +180,7 @@ public class Common {
     public static String profile;
     public static String profileImageUrl;
     public static String filename;
-    public static String regionsetting;
+    public static String regionSetting;
 
 
 }
