d7f65bffdcc6b0331f8e2167498d86301a826896
Brian0120 <tanaka2.taro5.10@gmail.com>
2019-12-29 13:11:01 +0900

    20191229_非活性ボタン作成済

--- app/src/main/java/twentyfour_seconds/com/del/event_management/EventManagementMaintenanceEventStatusChange.java
+++ app/src/main/java/twentyfour_seconds/com/del/event_management/EventManagementMaintenanceEventStatusChange.java
@@ -1,6 +1,8 @@
 package twentyfour_seconds.com.del.event_management;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,32 +21,35 @@ public class EventManagementMaintenanceEventStatusChange extends CustomActivity 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_management_maintenance_event_status_change);
 
+        // EventManagementListから引き継いできた情報を取得する
+            Intent intent = getIntent();
+            String event_status;
+            event_status = intent.getStringExtra("event_status");
+            final int ievent_status = Integer.parseInt(event_status);
+            Log.d("", event_status);
+
         // ビューの変数を初期化する
         RecruitmentResumedButton = findViewById(R.id.Resumed);
         RecruitmentStopButton = findViewById(R.id.Stop);
         GroupDissolutionButton = findViewById(R.id.Dissolution);
+        // イベント状態が"0"のとき、再開ボタン無効化
+        if (ievent_status == 0) {
+            RecruitmentResumedButton.setEnabled(false);
+        }
+        // イベント状態が"1"のとき、停止ボタン無効化
+        if (ievent_status == 1) {
+            RecruitmentStopButton.setEnabled(false);
+        }
 
         RecruitmentResumedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ボタンが押されたら無効化
-                RecruitmentResumedButton.setEnabled(false);
             }
         });
 
         RecruitmentStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // ボタンが押されたら無効化
-                RecruitmentStopButton.setEnabled(false);
-            }
-        });
-
-        GroupDissolutionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // ボタンが押されたら無効化
-                GroupDissolutionButton.setEnabled(false);
             }
         });
     }
